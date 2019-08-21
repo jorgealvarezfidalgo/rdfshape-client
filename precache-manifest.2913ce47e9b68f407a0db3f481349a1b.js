c4005d07ad35648c1bd64205ac927d41341a4295
Jose Labra <jelabra@gmail.com>
2019-08-21 08:49:40 +0200

    Updates

---
+++ precache-manifest.2913ce47e9b68f407a0db3f481349a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f4ba1360ea92177ced03c075f06a38",
+    "url": "/rdfshape-client/index.html"
+  },
+  {
+    "revision": "04d54745cc34958b1451",
+    "url": "/rdfshape-client/static/css/2.ba8d9e19.chunk.css"
+  },
+  {
+    "revision": "82cef0554c9d3e242404",
+    "url": "/rdfshape-client/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "04d54745cc34958b1451",
+    "url": "/rdfshape-client/static/js/2.bb262cc9.chunk.js"
+  },
+  {
+    "revision": "82cef0554c9d3e242404",
+    "url": "/rdfshape-client/static/js/main.3a11ebe2.chunk.js"
+  },
+  {
+    "revision": "41cedc7a9099b8c97c4c",
+    "url": "/rdfshape-client/static/js/runtime~main.c3e9eb09.js"
+  }
+]);
